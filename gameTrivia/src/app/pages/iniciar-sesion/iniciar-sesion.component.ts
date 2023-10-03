9b39094bded4eb26b7e622514c9b6ab027e92321
Digtester454 <141531017+Digtester454@users.noreply.github.com>
2023-10-03 14:29:14 -0500

    testeo1_Carlos

--- gameTrivia/src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
+++ gameTrivia/src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.component.html',
   styleUrls: ['./iniciar-sesion.component.css']
+  
 })
 export class IniciarSesionComponent {
   isLogin: boolean = true;
   cambiarBoolLogin(nuevoValor: boolean) {
     this.isLogin = nuevoValor;
   }
+
+ /*  constructor(private messageService: MessageService){
+
+  } */
+
+/*   show() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+} */
 
 }
